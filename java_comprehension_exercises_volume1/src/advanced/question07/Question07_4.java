322254f4045bccd1aba2867075fee2278a4661e2
yorikish <yorikish@C010423711.zipangu.cac.co.jp>
2026-04-16 13:55:44 +0900

    Q07_4完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_4 {
 
@@ -14,7 +16,28 @@ public class Question07_4 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+		for (int i = 0; i < 5; i++) {
+			System.out.print(itemNames[i] + "の購入数：>");
+			amounts[i] = Integer.parseInt(reader.readLine());
+		}
+		int price = 0;
+		int totalPrice = 0;
+
+		for (int i = 0; i < 5; i++) {
+			price = amounts[i] * prices[i];
+			System.out.println(itemNames[i] + "：" + price + "円");
+			totalPrice += price;
+		}
+		System.out.println("\n割引前合計：" + totalPrice + "円");
+		if (totalPrice >= 5000) {
+			totalPrice = (int) (totalPrice * 0.9);
+			System.out.println("割引適用（10%OFF）：" + totalPrice + "円");
+		} else {
+			System.out.println("割引はありません。");
+		}
+		System.out.println("税込合計：" + (int) (totalPrice * 1.1) + "円");
 	}
 
 }
